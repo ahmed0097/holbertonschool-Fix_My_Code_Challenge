ae75d22c3cb02f060038d0917fb66815a5dacb89
ahmed0097 <ahmed.zghal1233@gmail.com>
2022-10-02 22:03:14 +0100

    views

--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -6,7 +6,10 @@ from flask import jsonify
 from api.v1.views import app_views
 
 
-@app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
+"""
+Here is the error
+"""
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status():
     """ Status of the web server
     """
